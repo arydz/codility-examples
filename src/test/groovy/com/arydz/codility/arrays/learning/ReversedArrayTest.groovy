92f1b15616bb9dffb8b431eeb642598463c24595
arydz <driver89.adrian@gmail.com>
2018-03-30 19:39:41 +0200

    Improve tests for reversing array

--- src/test/groovy/com/arydz/codility/arrays/learning/ReversedArrayTest.groovy
+++ src/test/groovy/com/arydz/codility/arrays/learning/ReversedArrayTest.groovy
@@ -6,21 +6,13 @@ class ReversedArrayTest extends Specification {
 
     ReversedArray reversedArray = new ReversedArray()
 
-    def "test solution. Even length"() {
-        given:
-        int[] array = [1, 4, 3, 7]
-        when:
-        def result = reversedArray.solution(array)
-        then:
-        assert result == [7, 3, 4, 1]
+    def "Test solution"() {
+        expect:
+        expected == reversedArray.solution(*initArray)
+        where:
+        initArray       | expected
+        [1, 4, 3, 7]    | [7, 3, 4, 1]
+        [1, 4, 3, 7, 5] | [5, 7, 3, 4, 1]
     }
 
-    def "test solution. Odd length"() {
-        given:
-        int[] array = [1, 4, 3, 7, 5]
-        when:
-        def result = reversedArray.solution(array)
-        then:
-        assert result == [5, 7, 3, 4, 1]
-    }
 }
