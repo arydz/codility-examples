2eb69d9d36754c8aad401abfbbf679603e9fd23f
arydz <driver89.adrian@gmail.com>
2018-04-12 20:55:50 +0200

    Added solution for MissingInteger. Copied from demo example (other package)

---
+++ src/main/java/com/arydz/codility/countingelements/MissingInteger.java
@@ -0,0 +1,39 @@
+package com.arydz.codility.countingelements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingInteger {
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        int result = 1;
+        int expectedValue = 1;
+        List<Integer> positiveOnly = new ArrayList<>();
+        boolean expectedValuesInArray = true;
+        for (int i = 0 ; i < A.length; i++) {
+            if (A[i] > 0) {
+                positiveOnly.add(A[i]);
+            }
+        }
+
+        if (!positiveOnly.isEmpty()) {
+            for (int i = 0; i < positiveOnly.size(); i++) {
+                if (positiveOnly.get(i) == expectedValue) {
+                    expectedValue++;
+                }
+
+                if (positiveOnly.get(i) > expectedValue) {
+                    result = expectedValue;
+                    expectedValuesInArray = false;
+                }
+            }
+
+            if (expectedValuesInArray) {
+                result = expectedValue;
+            }
+        }
+        return result;
+    }
+}
